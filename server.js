cecf968c3255821115e897c051720ca99d7a8a6f
Abhishek R Gowda <abhiram201196mandya@gmail.com>
2025-09-03 11:00:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: "http://localhost:5000/api",
+        url: "https://myntra-clone-ulcv.onrender.com/",
       },
     ],
   },
